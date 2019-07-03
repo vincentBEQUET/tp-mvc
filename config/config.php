1fff7b456b49a5501402dbabb96e835104b4beae
Pierre-Louis MICHEL <michelpierrelouis48@gmail.com>
2019-07-03 14:03:47 +0200

    Re config de la connexion à la BD

--- config/config.php
+++ config/config.php
@@ -8,12 +8,12 @@ ini_set('allow_url_include', 1);
 
 const DB_HOST = 'localhost';
 const DB_PORT = '3306';
-const DB_NAME = 'blog';
+const DB_NAME = 'netflix';
 const DB_USER = 'root';
 const DB_PWD  = '';
 
 const WEBSITE_TITLE = "";
-const BASE_URL = "http://localhost/examen3";
+const BASE_URL = "http://localhost/tp-mvc";
 
 const CLASSES_SOURCES = [
     'src/controller',
