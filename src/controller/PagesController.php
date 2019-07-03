d815b0d6fc530df6d0aa57ab6bfcac7d7a14a4e9
Pierre-Louis MICHEL <michelpierrelouis48@gmail.com>
2019-07-03 14:26:17 +0200

    Creation controller

--- src/controller/PagesController.php
+++ src/controller/PagesController.php
@@ -2,11 +2,30 @@
 
 // PagesController.php
 
-class PagesController {
-
-    public function home() {
-
+class PagesController 
+{
+    public function home()
+    {
         view('pages.home');
+    }
+    
+    public function affichage_user() 
+    {
+        view('pages.affichage_user');
+    }
 
+    public function affichage_film()
+    {
+        view('pages.affichage_film');
+    }
+
+    public function add_user()
+    {
+        view('pages.add_user');
+    }
+
+    public function add_film()
+    {
+        view('pages.add_film');
     }
 }
