4ae38e31232ddf63191abd8eed961ed6adb9ec55
Pierre-Louis MICHEL <michelpierrelouis48@gmail.com>
2019-07-03 15:46:24 +0200

    Formulaire add_film

--- public/views/pages/add_film.php
+++ public/views/pages/add_film.php
@@ -1,12 +1,27 @@
 <?php ob_start(); ?>
-
+<?php
+//RECUPERATION DES DONNEES
+$request = 'SELECT * FROM type';
+$response = $bdd->query($request);
+$posts = $response->fetchAll(PDO::FETCH_ASSOC);
+?>
 <h1>Ajouter un film</h1>
 
-<form>
+<form method="post" enctype="multipart/form-data">
     <fieldset>
         <input type="text" id="title" placeholder="Titre du film">
-        <input type=""
-        <input type="text" id="name" placeholder="Nom de l'utilisateur">
+        <select id="type">
+            <?php
+            foreach ($posts as $post) //Affichage de tous les types.
+            {
+                echo "<option>" . $post['name'] . "</option>";
+            }
+            ?>
+        </select>
+        <label> Type du film </label>
+        <input type="text" id="author" placeholder="Auteur du film">
+        <input type="text" id="duration" placeholder="Durée du film">
+        <input type="file" name="photo">
         <input type="submit" id="ajout_film" value="Ajouter">
     </fieldset>
 </form>
