edb71033ab3bf1c79daca59fed1e917185671142
Pierre-Louis MICHEL <michelpierrelouis48@gmail.com>
2019-07-04 12:10:34 +0200

    Creation methode save UsersController

--- src/controller/UsersController.php
+++ src/controller/UsersController.php
@@ -10,17 +10,25 @@ class UsersController {
 
     public function add_user()
     {
-        /* A faire lorsque les POST soient complets. Entre les guimets du $_POST va nom du champ dans le formulaire
-        $user = new User;
-
-        $user->setName($_POST['']);
-        $user->setPassword($_POST['']); 
-        $user->setEmail($_POST['']); 
-        $user->setAvatar($_POST['']); 
-
-        $user->save();
-        */
         view('pages.add_user');
     }
 
+    public function save()
+    {
+        /* A faire lorsque les POST soient complets. Entre les guimets du $_POST va nom du champ dans le formulaire*/
+        if (!empty($_POST)) 
+        {
+            $user = new User;
+            
+            $user->setName($_POST['name']);
+            $user->setPassword($_POST['password']); 
+            $user->setEmail($_POST['email']); 
+            $user->setAvatar($_POST['avatar']); 
+            view('pages.add_user');
+            
+            $user->save();
+        }
+        
+        
+    }
 }
