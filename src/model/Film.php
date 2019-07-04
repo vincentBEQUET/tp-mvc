4d0f26d3e46e60e0610d6c56b204eb11e1da3ce6
Lluis <lluiso.aloha@gmail.com>
2019-07-04 10:35:09 +0200

    Vue Model + Validations

--- src/model/Film.php
+++ src/model/Film.php
@@ -1,6 +1,6 @@
 <?php
 
-class Film
+class Film extends Db
 {
 
     const TABLE_NAME =  'film';
@@ -41,23 +41,35 @@ class Film
 
     public function setAuthor($author)
     {
-        // Il faudra faire de validations
+        // Validations proposées
+        if (strlen($author) < 5) {
+            throw new Exception("Le nom de l'auteur est trop court.");
+        }
+        if (strlen($author) > 50) {
+            throw new Exception("Le nom de l'auteur est trop long.");
+        }
         $this->author = $author;
         return $this;
     }
     
     public function setPoster($poster)
     {
-        // Il faudra faire de validations
+        // Validations proposées
+        if (strlen($poster) < 5) {
+            throw new Exception("Le nom de l'auteur est trop court.");
+        }
+        if (strlen($poster) > 50) {
+            throw new Exception("Le nom de l'auteur est trop long.");
+        }
         $this->poster = $poster;
         return $this;
     }
     public function setReleaseYear($release_year) 
     {
-        // Validation
-        $aRegex = '/^19[0-9]{2}|(200[0-9]{1}|201[0-9]{1})$/'; //date doit être 19__ ou 200_ ou 201_
-        if ( !preg_match($aRegex, $release_year) ) {
-        throw new Exception('La date de création saisie n\'est pas valable.');
+        // Validation proposée
+        $current_date = date('Y');
+        if ($release_year < 1900 || $release_year > $current_date) {
+            throw new Exception("La date saisie n'est pâs valable");
         }
         $this->release_year = $release_year;
         return $this;
@@ -65,13 +77,24 @@ class Film
     
     public function setMovieDuration($duration)
     {
-        // Il faudra faire de validations
+        // Validation proposée
+        if ($duration < 1800) {
+            throw new Exception("La durée d'un film doit être superieur à 30 min, donc 1800 secondes.");
+        }
+        if ($duration > 14400) {
+            throw new Exception( "La durée d'un film doit être inferieur à 4 heures, donc 14400 seconds.");
+        }
         $this->duration = $duration;
         return $this;
     }
     public function setGif($gif)
     {
-        // Il faudra faire de validations
+        if (strlen($gif) < 5) {
+            throw new Exception("Le nom du GIF est trop court.");
+        }
+        if (strlen($gif) < 255) {
+            throw new Exception("Le nom du GIF est trop long.");
+        }
         $this->gif = $gif;
         return $this;
     }
