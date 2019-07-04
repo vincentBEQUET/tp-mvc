4c8688774b17c4c5d083eaa046a3a7acabc5ccbd
Pierre-Louis MICHEL <michelpierrelouis48@gmail.com>
2019-07-04 10:46:13 +0200

    Enregistrement film (non fonctionnel)

--- src/controller/FilmsController.php
+++ src/controller/FilmsController.php
@@ -11,19 +11,23 @@ class FilmsController
 
     public function add_film()
     {
-        /*   A faire lorsque les POST soient complets. Entre les guimets du $_POST va nom du champ dans le formulaire
-        $film = new Film;
+           //A faire lorsque les POST soient complets. Entre les guimets du $_POST va nom du champ dans le formulaire
 
-        $film->setTitle($_POST['']);
-        $film->setTypeId($_POST['']);
-        $film->setAuthor($_POST['']);
-        $film->setPoster($_POST['']);
-        $film->setReleaseYear($_POST['']);
-        $film->setMovieDuration($_POST['']);
-        $film->setGif($_POST['']);
+        if (!empty ($_POST))
+        {
+            $film = new Film;
 
-        $film->save();
-*/
+            $film->setTitle($_POST['']);
+            $film->setTypeId($_POST['']);
+            $film->setAuthor($_POST['']);
+            $film->setPoster($_POST['']);
+            $film->setReleaseYear($_POST['']);
+            $film->setMovieDuration($_POST['']);
+            $film->setGif($_POST['']);
+
+            $film->save();
+        }
+        
         $types = Type::findAll();
         view('pages.add_film', compact('types')); // Recherche des types de film pour le champ type du formulaire de création de film.
     }
