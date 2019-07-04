ebcba5e1f2a7b315acb24733f0dbef95ad581bed
BEQUET <vincent.bequet@hotmail.fr>
2019-07-04 10:31:53 +0200

    10h30

--- src/controller/PagesController.php
+++ src/controller/PagesController.php
@@ -9,26 +9,7 @@ class PagesController
         view('pages.home');
     }
 
-    public function affichage_user()
-    {
-        $users = User::findAll();
-        view('pages.affichage_user', compact('users'));
-    }
-
-    public function add_user()
-    {
-        /* A faire lorsque les POST soient complets. Entre les guimets du $_POST va nom du champ dans le formulaire
-        $user = new User;
-
-        $user->setName($_POST['']);
-        $user->setPassword($_POST['']); 
-        $user->setEmail($_POST['']); 
-        $user->setAvatar($_POST['']); 
-
-        $user->save();
-        */
-        view('pages.add_user');
-    }
+    
 
     public function affichage_film()
     {
