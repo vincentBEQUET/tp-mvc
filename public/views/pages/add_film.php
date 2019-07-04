ba557752d0c30524307adc46e8e338c46f7ba0d0
Lluis <lluiso.aloha@gmail.com>
2019-07-04 11:46:53 +0200

    Fix film form

--- public/views/pages/add_film.php
+++ public/views/pages/add_film.php
@@ -7,11 +7,12 @@
         <input name="title" type="text" id="title" placeholder="Titre du film"><br><br>
         <select name="type" id="type">
             <?php foreach ($types as $type) : ?>
-                <option value="<?= $type['id'] ?>"><?= $type['name'] ?></option>            
+                <option value="<?= $type['id'] ?>"><?= $type['name'] ?></option>
             <?php endforeach ?>
         </select>
         <label> Type du film </label><br><br>
         <input name="author" type="text" id="author" placeholder="Auteur du film"><br><br>
+        <input name="release_year" type="text" id="release_year" placeholder="Année de sortie"><br><br>
         <input name="duration" type="text" id="duration" placeholder="Durée du film"><br><br>
         <input name="poster" type="file"><br><br>
         <input type="submit" id="ajout_film" value="Ajouter"><br><br>
